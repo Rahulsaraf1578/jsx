dec4a0a1b30dcd569297b67495c976249491a19f
Rahulsaraf <63157871+Rahulsaraf1578@users.noreply.github.com>
2023-07-04 16:55:25 +0530

    Writing shorthand jsx

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from 'react'
 
 function App() {
 
+    // Showing dynamic content
     let message = 'Bye there!'
 
     if(Math.random(0,1)>0.5){
         message =  'Hello there!'
     }
 
+    // Writing shorthand jsx
+    const time = new Date().toLocaleTimeString();
+
     return (
-        <div>{message}</div>
+        <div>
+            <div> {message} </div>
+            <div> {time} </div>
+        </div>
     )
 }
 
